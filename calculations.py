108d8decd8190791880549db37cafb685d0e0401
tom-butterworth <2410306@stu.chi.ac.uk>
2025-11-18 12:10:18 +0000

    Made functions for all mandatory calculation & display, and for some optional display suggestions from assignment brief. Also created plots.py to be used for graph code. Adding student_grades.csv to the commit too.

--- calculations.py
+++ calculations.py
@@ -1,3 +1,41 @@
 #load csv data into a dataframe
 from load_data import load_csv
 df = load_csv()
+
+#get mean grade
+def calculate_average_grade(df):
+    return df["grade"].mean()
+
+#get mean attendance
+def calculate_average_attendance(df):
+    return df["attendance"].mean()
+
+#get number of passes (grades above 40)
+def get_number_of_passes(df):
+    return len(df[df["grade"] >= 40])
+
+#get number of fails (grades below 40)
+def get_number_of_fails(df):
+    return len(df[df["grade"] < 40])
+
+#get number of As (grades > 70)
+def get_number_of_As(df):
+    return len(df[df["grade"] >= 70])
+
+#get number of Bs (grades between 60 and 70)
+def get_number_of_Bs(df):
+    return len(df[df["grade"].between(60, 70, inclusive="left")]) #inclusive left means lower bound is inclusive, upper bound is exclusive
+
+#get number of Cs (grades between 40 and 60)
+def get_number_of_Cs(df):
+    return len(df[df["grade"].between(40, 60, inclusive="left")])
+
+#gets the average grade for each country
+def get_average_grade_per_country(df):
+    return df.groupby("country")["grade"].mean()
+
+def get_correlation_attendance_and_grade(df):
+    return df["attendance"].corr(df["grade"])
+
+def get_general_correlation(df):
+    return df.corr()
--- main.py
+++ main.py
@@ -1,3 +1,24 @@
+from calculations import * #import all functions from calculations.py for us to use later
+from plots import * #import everything from plots.py to display graphs later
+
 #load csv data into a dataframe
-from load_data import load_csv
-df = load_csv()
+# from load_data import load_csv
+# df = load_csv()
+
+print(f"Average grade: {calculate_average_grade(df)}")
+
+print(f"Average attendance: {calculate_average_attendance(df)}")
+
+print(f"Number of fails: {get_number_of_fails(df)}")
+
+print(f"Number of passes: {get_number_of_passes(df)}")
+
+print(f"Number of As: {get_number_of_As(df)}")
+
+print(f"Number of Bs: {get_number_of_Bs(df)}")
+
+print(f"Number of Cs: {get_number_of_Cs(df)}")
+
+print(f"Average grade by country: {get_average_grade_per_country(df)}")
+
+print(f"Correlation between attendance and grade: {get_correlation_attendance_and_grade(df)}")
