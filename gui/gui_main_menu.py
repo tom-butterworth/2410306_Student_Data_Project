75ed2a50d8e4a301d28e5242069458aad79c035d
tom-butterworth <2410306@stu.chi.ac.uk>
2025-12-05 10:47:59 +0000

    Edit buttons to be ttk buttons rather than older tk

--- gui/gui_main_menu.py
+++ gui/gui_main_menu.py
@@ -1,9 +1,10 @@
 import tkinter as tk #used to create gui
 from gui_helpers import centre_window
 from gui_statistics import open_statistics_gui
+from tkinter import ttk #use for widgets if possible, modern widgets have more formatting and features than older tk widgets
 
 #set up a tkinter window
-#from research/reading, it's convention to use root as the name of the window/root for a tkinter gui, so I'll use that for my main menu
+#from research, it's convention to use root as the name of the window/root for a tkinter gui, so I'll use that for my main menu
 root = tk.Tk()
 root.title("Student Data")
 centre_window(root, 600, 150) #call function to centre window
@@ -18,9 +19,9 @@ def open_student_search_gui():
     pass
 
 
-tk.Button(root, text="Display Statistics", command=open_statistics_gui).grid(row=1, column=0, padx=10, pady=15)
-tk.Button(root, text="Display Graphs", command=open_graphs_gui).grid(row=1, column=1, padx=10, pady=15)
-tk.Button(root, text="Search Students", command=open_student_search_gui).grid(row=1, column=2, padx=10, pady=15)
+ttk.Button(root, text="Display Statistics", command=open_statistics_gui).grid(row=1, column=0, padx=10, pady=15)
+ttk.Button(root, text="Display Graphs", command=open_graphs_gui).grid(row=1, column=1, padx=10, pady=15)
+ttk.Button(root, text="Search Students", command=open_student_search_gui).grid(row=1, column=2, padx=10, pady=15)
 
 root.grid_columnconfigure(0, weight=1)
 root.grid_columnconfigure(1, weight=1)
