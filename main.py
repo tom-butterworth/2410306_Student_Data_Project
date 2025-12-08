7c55fe0f565665c800662ee3652111d20fb11b5a
tom-butterworth <2410306@stu.chi.ac.uk>
2025-12-08 18:18:49 +0000

    Change tweaked imports

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-from db_conn import get_connection, create_database #returns database connection so we can query it
+from db_conn import create_database #returns database connection so we can query it
 from load_data import load_csv #to be used to create dataframe used when calling calculations etc
-from calculations import * #import all functions from calculations.py for us to use later
 from gui.gui_main_menu import open_main_menu
 
 #create a dataframe from the student grades csv file
