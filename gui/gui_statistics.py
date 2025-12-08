e9447452af613b1c009e2292279cfa8f2e2449ff
tom-butterworth <2410306@stu.chi.ac.uk>
2025-12-08 15:27:50 +0000

    Add buttons for remaining mandatory calculations. Change buttons from tk to ttk.

--- gui/gui_statistics.py
+++ gui/gui_statistics.py
@@ -1,7 +1,8 @@
 import tkinter as tk
+from tkinter import ttk
 from tkinter import messagebox
 from db_conn import get_connection
-from calculations import calculate_average_grade_sql, calculate_average_attendance_sql
+from calculations import calculate_average_grade_sql, calculate_average_attendance_sql, get_number_of_passes_sql, get_number_of_fails_sql, get_number_of_As_sql, get_number_of_Bs_sql, get_number_of_Cs_sql
 from gui.gui_helpers import centre_window
 
 #open and display the window. Everything related to building and using this window needs to be in the function.
@@ -33,8 +34,13 @@ def open_statistics_gui():
     You can think of it like a recipe vs making the finished food. Tkinter wants the recipe for later, so wrapping it in a lambda function gives tkinter a reference to this recipe, rather than ...
     ... telling it to make the food straight away.
     """
-    tk.Button(win_statistics, text="Average Grade", command=lambda: show_result("Average Grade", calculate_average_grade_sql(conn))).pack(padx=10, pady=5, anchor="w")
-    tk.Button(win_statistics, text="Average Attendance", command=lambda: show_result("Average Attendance", calculate_average_attendance_sql(conn))).pack(padx=10, pady=5, anchor="w")
+    ttk.Button(win_statistics, text="Average Grade", command=lambda: show_result("Average Grade", calculate_average_grade_sql(conn))).pack(padx=10, pady=5, anchor="w")
+    ttk.Button(win_statistics, text="Average Attendance", command=lambda: show_result("Average Attendance", calculate_average_attendance_sql(conn))).pack(padx=10, pady=5, anchor="w")
+    ttk.Button(win_statistics, text="Number of Passes", command=lambda: show_result("Number of Passes", get_number_of_passes_sql(conn))).pack(padx=10, pady=5, anchor="w")
+    ttk.Button(win_statistics, text="Number of Fails", command=lambda: show_result("Number of Fails", get_number_of_fails_sql(conn))).pack(padx=10, pady=5, anchor="w")
+    ttk.Button(win_statistics, text="Number of As", command=lambda: show_result("Number of As", get_number_of_As_sql(conn))).pack(padx=10, pady=5, anchor="w")
+    ttk.Button(win_statistics, text="Number of Bs", command=lambda: show_result("Number of Bs", get_number_of_Bs_sql(conn))).pack(padx=10, pady=5, anchor="w")
+    ttk.Button(win_statistics, text="Number of Cs", command=lambda: show_result("Number of Cs", get_number_of_Cs_sql(conn))).pack(padx=10, pady=5, anchor="w")
 
     #function to be called when the window is closed, handles closing db connection and destroying the window
     def on_close():
@@ -45,3 +51,10 @@ def open_statistics_gui():
             messagebox.showerror("Error", f"Error: {e}")
 
     win_statistics.protocol("WM_DELETE_WINDOW", on_close) #WM_DELETE_WINDOW is the event triggered by clicking the close button. When this is triggered, call on_close() allowing cleanup before destroying the window
+
+#for testing
+if __name__ == "__main__":
+    root = tk.Tk() #create a root window
+    root.withdraw() #hide it if you only want the statistics window
+    open_statistics_gui()
+    root.mainloop()
