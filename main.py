e243783f1720fd1a7f59182b20ffae96c7eb1910
tom-butterworth <2410306@stu.chi.ac.uk>
2025-12-08 18:16:40 +0000

    Change removed all old console/terminal outputs

--- main.py
+++ main.py
@@ -12,30 +12,3 @@ create_database()
 #open the main menu window
 open_main_menu()
 
-#using our database connection, call functions to calculate and display information
-with get_connection() as conn:
-    # print(f"Average grade: {calculate_average_grade(df)}")
-    print(f"Average grade from SQL: {calculate_average_grade_sql(conn)}\n")
-
-    # print(f"Average attendance: {calculate_average_attendance(df)}")
-    print(f"Average attendance from SQL: {calculate_average_attendance_sql(conn)}\n")
-
-    # print(f"Number of passes: {get_number_of_passes(df)}")
-    print(f"Number of passes from SQL: {get_number_of_passes_sql(conn)}\n")
-
-    # print(f"Number of fails: {get_number_of_fails(df)}")
-    print(f"Number of fails from SQL: {get_number_of_fails_sql(conn)}\n")
-
-    # print(f"Number of As: {get_number_of_As(df)}")
-    print(f"Number of As from SQL: {get_number_of_As_sql(conn)}\n")
-
-    # print(f"Number of Bs: {get_number_of_Bs(df)}")
-    print(f"Number of Bs from SQL: {get_number_of_Bs_sql(conn)}\n")
-
-    # print(f"Number of Cs: {get_number_of_Cs(df)}")
-    print(f"Number of Cs from SQL: {get_number_of_Cs_sql(conn)}\n")
-
-    # print(f"Average grade by country:\n{get_average_grade_per_country(df)}")
-    print(f"Average grade by country from SQL:\n{get_average_grade_per_country_sql(conn)}\n")
-
-    print(f"Correlation between attendance and grade: {get_correlation_attendance_and_grade(df)}")
