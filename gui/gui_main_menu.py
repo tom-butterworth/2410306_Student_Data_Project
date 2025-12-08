43c4d120d7d3676a943d02b07f8486748c866c83
tom-butterworth <2410306@stu.chi.ac.uk>
2025-12-08 18:25:43 +0000

    Change main menu now opens as a toplevel window from root

--- gui/gui_main_menu.py
+++ gui/gui_main_menu.py
@@ -1,17 +1,16 @@
 import tkinter as tk #used to create gui
-from gui.gui_helpers import centre_window
+from gui.gui_helpers import centre_window, safe_close_window
 from gui.gui_statistics import open_statistics_gui
 from tkinter import ttk #use for widgets if possible, modern widgets have more formatting and features than older tk widgets
 from sv_ttk import get_theme, set_theme  # will be used to set a gui theme (windows 11 like theme with light and dark variants)
 
 
-def open_main_menu():
-    #set up a tkinter window
-    #from research, it's convention to use root as the name of the window/root for a tkinter gui, so I'll use that for my main menu
-    root = tk.Tk()
-    root.title("Student Data")
-    centre_window(root, 600, 150) #call function to centre window
-    ttk.Label(root, text="SID - Student Information Data", font=("Arial", 15, "bold")).grid(row=0, column=0, columnspan=3, pady=20) #columnspan and rowspan used to make content span multiple cells of a grid
+def open_main_menu(root):
+    win_main_menu = tk.Toplevel(root) #creates a toplevel window on top of the current window, attached to the root window from main.py
+    win_main_menu.title("Main Menu")
+    centre_window(win_main_menu, 600, 150) #call function to centre the window
+    #below: columnspan and rowspan used to make content span multiple cells of a grid. tk.Label rather than ttk.Label as ttk.Label applies a grey background and is awkward with themes applied
+    tk.Label(win_main_menu, text="SID - Student Information Data", font=("Arial", 15, "bold")).grid(row=0, column=0, columnspan=3, pady=20)
 
 
 
