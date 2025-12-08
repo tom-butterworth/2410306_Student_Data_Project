67a3cd747a61420a134eaa20a98d67044af02851
tom-butterworth <2410306@stu.chi.ac.uk>
2025-12-08 18:11:00 +0000

    Add setup_styles function to apply a theme to our gui, with example of configuring a custom style too

---
+++ gui/styles.py
@@ -0,0 +1,14 @@
+from tkinter import ttk
+import sv_ttk
+
+def setup_styles(root, theme="light"):
+    sv_ttk.set_theme(theme)
+    style = ttk.Style(root) #needed for if you want to use custom styles. Applies automatically to any TopLevel windows
+
+    #Example of how to configure a custom tkinter style
+    style.configure(
+        "Title.TLabel",
+        font=("Arial", 12, "bold"),
+        foreground="blue",
+        background="F0F0F0",
+    )
