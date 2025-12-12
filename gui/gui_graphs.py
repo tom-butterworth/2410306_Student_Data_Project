82e567df53d128a46b7cf22235525a40bbfe4a5f
tom-butterworth <2410306@stu.chi.ac.uk>
2025-12-12 12:18:04 +0000

    Add start building a graph gui. User selects graphs to show

---
+++ gui/gui_graphs.py
@@ -0,0 +1,24 @@
+import tkinter as tk
+from gui.gui_helpers import safe_close_window, centre_window
+from tkinter import ttk
+from gui.plots import plot_grade_vs_attendance, plot_average_grade_by_country
+from load_data import load_csv
+
+#Get a dataframe from the csv file for us to use when calling plot functions
+df = load_csv()
+
+
+#open and display the window. Everything related to building and using this window needs to be in the function.
+def open_graphs_gui():
+    win_graphs = tk.Toplevel() #create a toplevel window
+    win_graphs.title("Graphs")
+    centre_window(win_graphs, 600, 400)
+
+    #without lambda functions, graphs would show instantly when win_graphs opens, even if the user hasn't clicked a button yet
+    ttk.Button(win_graphs, text="Grade vs Attendance", command=lambda: plot_grade_vs_attendance(df)).grid(row=0, column=0, padx=10, pady=10)
+    ttk.Button(win_graphs, text="Average grade by country", command=lambda: plot_average_grade_by_country(df)).grid(row=1, column=0, padx=10, pady=10)
+
+
+
+
+    win_graphs.protocol("WM_DELETE_WINDOW", lambda: safe_close_window(win_graphs))
