d1f7fdeabc0cac1f510b40a5e67e1b47ec11ee01
tom-butterworth <2410306@stu.chi.ac.uk>
2025-12-12 14:03:19 +0000

    Add graphs clear when a new one is selected

--- gui/plots.py
+++ gui/plots.py
@@ -1,12 +1,18 @@
 import matplotlib.pyplot as plt
+import numpy as np
+
+def clearplots():
+    plt.clf() #clears all plots from the current figure. This means new plots appear in the same window, rather than opening a new window
 
 #Correlation between grade and attendance
 def plot_grade_vs_attendance(df):
+    clearplots()
     plt.scatter(df["attendance"], df["grade"])
     plt.show()
 
 #Bar chart of average grade per country
 def plot_average_grade_by_country(df):
+    clearplots()
     avg_grades = df.groupby("country")["grade"].mean() #group by country and calculate average grade for each country
     avg_grades.sort_values(ascending=False, inplace=True) #need inplace=True to directly change the avg_grades dataframe, otherwise it returns a modified df for later instead of using it
     plt.bar(avg_grades.index, avg_grades.values)
