96b8a454e4be10a3dafedccb650a09093f84e38d
tom-butterworth <2410306@stu.chi.ac.uk>
2025-12-15 18:08:41 +0000

    Change treeview styling for readability

--- gui/styles.py
+++ gui/styles.py
@@ -5,10 +5,26 @@ def setup_styles(theme="light"):
     sv_ttk.set_theme(theme)
     style = ttk.Style() #needed for if you want to use custom styles. Applies automatically to any TopLevel windows
 
-    #Example of how to configure a custom tkinter style
     style.configure(
-        "Title.TLabel",
-        font=("Arial", 12, "bold"),
-        foreground="blue",
-        background="F0F0F0",
+        "Treeview",
+        background="#2b2b2b", #dark grey background
+        foreground="white", #white text
+        fieldbackground="#2b2b2b", #for empty space
+        rowheight=25 #makes rows bigger
     )
+
+    style.configure(
+        "Treeview.Heading",
+        background="#2b2b2b",
+        foreground="white",
+        fieldbackground="#2b2b2b",
+    )
+
+
+    #Example of how to configure a custom tkinter style
+    # style.configure(
+    #     "Title.TLabel",
+    #     font=("Arial", 12, "bold"),
+    #     foreground="blue",
+    #     background="F0F0F0",
+    # )
