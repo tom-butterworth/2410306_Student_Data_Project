1b08bf8d38ce486d9fc6fa3d6fe3016f4ba9034e
tom-butterworth <2410306@stu.chi.ac.uk>
2025-12-15 17:55:34 +0000

    Change to dark theme, tweak styling function

--- gui/styles.py
+++ gui/styles.py
@@ -1,9 +1,9 @@
 from tkinter import ttk
 import sv_ttk
 
-def setup_styles(root, theme="light"):
+def setup_styles(theme="light"):
     sv_ttk.set_theme(theme)
-    style = ttk.Style(root) #needed for if you want to use custom styles. Applies automatically to any TopLevel windows
+    style = ttk.Style() #needed for if you want to use custom styles. Applies automatically to any TopLevel windows
 
     #Example of how to configure a custom tkinter style
     style.configure(
